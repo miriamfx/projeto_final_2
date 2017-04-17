c92debe8b569215ab2ee905d2c190a8f8a0e4be4
miriamfx <miriamfx2@gmail.com>
2017-04-17 14:06:55 -0300

    Update manager.py

--- source/manager.py
+++ source/manager.py
@@ -10,42 +10,42 @@ import dbmanager
 import dbquery
 import time
 
-
-def opt1(self, ip, community):
-    ''' Cadastro manual de Hosts. '''
-    l = []
-    if (main.ip != '') and (main.community != ''):
-        l.append((main.ip, main.comnunity))
-        laux = dbmanager.reg_query_db('{0}{1}'.format(dbmanager.DB), l)
-        dbmanager.reg_hosts_db('{0}{1}'.format(dbmanager.DB), laux)
-    else:
-        main.resultado = ('IP ou Comunidade vazio nao sao validos')
-
-
-def opt2(self, ip, community):
-    ''' Faz uma chamada no coletor manual de informacoes que ira coletar as informacoes dos Hosts.'''
-    subprocess.call('./get.py')
+class gerente():
+    def opt1(self, ip, community):
+        ''' Cadastro manual de Hosts. '''
+        l = []
+        if (main.ip != '') and (main.community != ''):
+            l.append((main.ip, main.comnunity))
+            laux = dbmanager.reg_query_db('{0}{1}'.format(dbmanager.DB), l)
+            dbmanager.reg_hosts_db('{0}{1}'.format(dbmanager.DB), laux)
+        else:
+            main.resultado = ('IP ou Comunidade vazio nao sao validos')
 
 
-def opt3(self, ip, community, min, sec):
-    ''' Executa o agendamento do collector, que ira coletar informacoes de gerenciamento dos hosts cadastrados. '''
-    x = 0
-    if x < (min / 60):
-        time.sleep(sec)
+    def opt2(self, ip, community):
+        ''' Faz uma chamada no coletor manual de informacoes que ira coletar as informacoes dos Hosts.'''
+        subprocess.call('./get.py')
 
-def opt4(self,ip):
-    pass
 
-def opt5(self, ip):
-    ''' Realiza a Exclusao de Hosts '''
-    if (main.ip != ''):
-        dbmanager.del_hosts_db('{0}{1}'.format(dbmanager.DB), dbmanager.l)
-    else:
+    def opt3(self, ip, community, min, sec):
+        ''' Executa o agendamento do collector, que ira coletar informacoes de gerenciamento dos hosts cadastrados. '''
+        x = 0
+        if x < (min / 60):
+            time.sleep(sec)
 
-        dbmanager.drop_tables_db
-        dbmanager.create_tables_db
+    def opt4(self,ip):
+        pass
+
+    def opt5(self, ip):
+        ''' Realiza a Exclusao de Hosts '''
+        if (main.ip != ''):
+            dbmanager.del_hosts_db('{0}{1}'.format(dbmanager.DB), dbmanager.l)
+        else:
+
+            dbmanager.drop_tables_db
+            dbmanager.create_tables_db
 
 
 if __name__ == '__main__':
-    return
+   gerente()
 
