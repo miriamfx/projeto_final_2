975b7c9f5153bc7678647b9f307d0881401575a8
miriamfx <miriamfx2@gmail.com>
2017-04-19 15:38:08 -0300

    Add files via upload

---
+++ sources/settings.py
@@ -0,0 +1,17 @@
+# Porta padrao do protocolo SNMP. Padrao 161
+DEFAULT_PORT = 161
+
+# Comunidade utilizada no SNMP. Padrao public
+DEFAULT_COMMUNITY = 'public'
+
+# Campo padrao para localizacao de Hosts com SNMP ativo.
+DEFAULT_FIELD = 'sysName'
+
+# Versao do protocolo SNMP. Padrao SNMPv2-MIB
+SNMP_VERSION = 'SNMPv2-MIB'
+
+# Caminho do banco de dados.
+DB = '/Documentos/Projeto_final2/sources/database/snmpdb.sqlite'
+
+# Caminho do diretorio de logs local.
+LOG_FOLDER = '/Documentos/Projeto_final2/sources/logs/'
